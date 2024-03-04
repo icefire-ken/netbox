9014b07f6c9a03f909cd9814af6f2763533f544c
Ken.Tang <IceFire_Ken@163.com>
2024-03-04 13:07:25 +0800

    2024.03.04

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# netbox
+# Docker安装NetBox（带有Plugins）
+https://github.com/netbox-community/netbox-docker/wiki/Using-Netbox-Plugins
+
+## Step 1
+
+## Step 1
+
+## Step 1
+
+## Step 4
+docker compose build --no-cache
+docker compose up -d
+
+## Step 5
+docker compose exec netbox /opt/netbox/netbox/manage.py createsuperuser
--- plugins.py
+++ plugins.py
@@ -13,36 +13,46 @@
 # }
 
 PLUGINS = ["netbox_dns",
-           "netbox_bgp",
            "netbox_secrets",
            "netbox_floorplan",
-           "netbox_acls",
            "netbox_topology_views",
            "netbox_qrcode",
-           "netbox_prometheus_sd",
            "netbox_lifecycle",
            "netbox_inventory",
-           "nb_risk",
            "netbox_routing",
            "validity",
            "netbox_documents",
-           "netbox_lists",
-           "netbox_kea",
            "netbox_napalm_plugin",
-           "netbox_proxbox"]
+           "netbox_vlan_manager"]
 
 PLUGINS_CONFIG = {
-    "netbox_acls": {
-        "top_level_menu": True
-    },
     "netbox_inventory": {},
     "netbox_napalm_plugin": {
         "NAPALM_USERNAME": "xxx",
         "NAPALM_PASSWORD": "yyy",
     },
-    "nb_risk": {
-        "additional_assets": [
-            "app_label.model_name",
-        ],
+    "netbox_documents": {
+        # Enable the management of site specific documents (True/False)
+        'enable_site_documents': True,
+        # Enable the management of location specific documents (True/False)
+        'enable_location_documents': True,
+        # Enable the management of circuit specific documents (True/False)
+        'enable_circuit_documents': True,
+        # Enable the management of device specific documents (True/False)
+        'enable_device_documents': True,
+        # Enable the management of device type specific documents (True/False)
+        'enable_device_type_documents': True,
+        # Enable the global menu options (True/False)
+        'enable_navigation_menu': True,
+        # Location to inject the document widget in the site view (left/right)
+        'site_documents_location': 'left',
+        # Location to inject the document widget in the location view (left/right)
+        'location_documents_location': 'left',
+        # Location to inject the document widget in the device view (left/right)
+        'device_documents_location': 'left',
+        # Location to inject the document type widget in the device type view (left/right)
+        'device_type_documents_location': 'left',
+        # Location to inject the document widget in the device view (left/right)
+        'circuit_documents_location': 'left'
     },
 }
